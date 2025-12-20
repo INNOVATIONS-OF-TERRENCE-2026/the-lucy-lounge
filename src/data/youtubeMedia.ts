5b47ee5f176f5e30e3568de06b78e8445d161114
EXPRESS AUDIO POWER COMPANY <Millinershelpingtheyouth@gmail.com>
2025-12-20 02:38:55 -0600

    Create youtubeMedia.ts with playlists and singles
    Added YouTube playlists and singles data.

---
+++ src/data/youtubeMedia.ts
@@ -0,0 +1,68 @@
+export const YOUTUBE_PLAYLISTS = [
+  {
+    title: "Free Family Movies Collection",
+    playlistId: "PLCriYrweK0098Mtl95jNTBNPdgGj26RXK",
+  },
+  {
+    title: "Free Movies & Shows",
+    playlistId: "PLX9_I-EOJPdHZJDzvjjRjpj86ClhZSsVm",
+  },
+  {
+    title: "Nicktoons Full Episodes",
+    playlistId: "PLUQR09yEYrP0RaHE3f9vNQkOx08IT9ZTe",
+  },
+  {
+    title: "CatDog Full Episodes",
+    playlistId: "PLfrgt_xI4Xq2Cg78vsEjqEs-aJv1HQmO0",
+  },
+  {
+    title: "The Fresh Prince of Bel-Air",
+    playlistId: "PLRDC-DZ_uWhrOMCryr5YU--MrsZPL9sb3",
+  },
+  {
+    title: "Classic Cartoons Collection",
+    playlistId: "PLx78q5BVTkpxxgNBfyMHWEyJRRhiw01Un",
+  },
+  {
+    title: "Animaniacs – Pinky & The Brain",
+    playlistId: "PLd3-CCCcbQeJSROdd9rf5YV996HwYdEDL",
+  },
+  {
+    title: "Animaniacs – Warner Bros Kids",
+    playlistId: "PLfrgt_xI4Xq0GvPClEj57Xhl1vt8Ww5fZ",
+  },
+  {
+    title: "Courage the Cowardly Dog",
+    playlistId: "PLLIU9nFd9IrGmATWUdDgpsMzTAMgDXrNp",
+  },
+];
+
+export const YOUTUBE_SINGLES = [
+  { title: "Casper's Haunted Christmas", videoId: "hr2rI0qn5EA" },
+  { title: "SpongeBob Classic Marathon", videoId: "8B8jplhrlso" },
+  { title: "SpongeBob Season 2", videoId: "pr80hyFCeGY" },
+  { title: "Ed, Edd n Eddy – The Eds Are Coming", videoId: "X-HRLChOTOA" },
+  { title: "Courage – Muriel’s Swampy Love Story", videoId: "uAVQgEXrZZU" },
+  { title: "Dexter’s Laboratory", videoId: "3bLNQgRn-Wg" },
+  { title: "Powerpuff Girls", videoId: "c0KlvkCKpE4" },
+  { title: "Recess (Disney)", videoId: "-UtUuT8AJjQ" },
+  { title: "That’s So Raven – Full Episode", videoId: "Tr7FcIvjVc4" },
+  { title: "Django", videoId: "ZxHLuzOnVNo" },
+  { title: "First Sunday", videoId: "3Aky7idipRk" },
+  { title: "Norbit", videoId: "-lbDPdksl-E" },
+  { title: "Gotti", videoId: "twW1G_yji5Q" },
+  { title: "Dumb and Dumber To", videoId: "TTgsopmTDG8" },
+  { title: "ATL", videoId: "ybzh6_5GFD0" },
+  { title: "The New Edition Story", videoId: "Iz7PQsBYM7g" },
+  { title: "Bonus Movie 1", videoId: "di2rSFQ-AvA" },
+  { title: "Bonus Movie 2", videoId: "f-Qnyvek5rE" },
+  { title: "Bonus Movie 3", videoId: "vBzxrvTl-ik" },
+  { title: "Bonus Movie 4", videoId: "Xs3YxSIRVi8" },
+  { title: "Bonus Movie 5", videoId: "mhcMts3LdWA" },
+  { title: "Bonus Movie 6", videoId: "22W83FgIX54" },
+  { title: "Bonus Movie 7", videoId: "JmKS9WCTuZg" },
+  { title: "Bonus Movie 8", videoId: "LRsBEtYNffU" },
+  { title: "Bonus Movie 9", videoId: "57YMa7kaZCA" },
+  { title: "Bonus Movie 10", videoId: "jKzn3qyq7Yk" },
+  { title: "Bonus Movie 11", videoId: "A2J1Q1fa3Rg" },
+];
