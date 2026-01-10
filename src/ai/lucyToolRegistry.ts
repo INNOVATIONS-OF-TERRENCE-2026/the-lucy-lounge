3efa4c7410fc377489176bbc2f6d7e25537753f2
EXPRESS AUDIO POWER COMPANY <Millinershelpingtheyouth@gmail.com>
2026-01-10 02:30:30 -0600

    Create lucyToolRegistry.ts

---
+++ src/ai/lucyToolRegistry.ts
@@ -0,0 +1,7 @@
+export const LucyTools = {
+  generate_video: {
+    name: "generate_video",
+    description: "Generate cinematic AI video",
+    endpoint: "/functions/v1/lucy-generate-video",
+  },
+};
